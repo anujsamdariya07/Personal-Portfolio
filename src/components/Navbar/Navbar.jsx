403dd3630e2ebde9683ebe93f608f58f6975ed99
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-06-11 19:58:30 +0530

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../../assets/logo.svg'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <h1 className='logo' >ANUJ</h1>
+      <ul className='nav-menu'>
+        <li>HOME</li>
+        <li>ABOUT ME</li>
+        <li>SERVICES</li>
+        <li>PORTFOLIO</li>
+        <li>CONTACT</li>
+      </ul>
+      <button className='nav-connect'>Connect With Me</button>
+    </div>
+  )
+}
+
+export default Navbar
