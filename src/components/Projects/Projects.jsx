e0385bb1b46201e7b9314fc83cf179496553ae8e
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-08-31 12:12:37 +0530

    Updating projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,33 +9,39 @@ function Projects() {
         <h1>My Projects</h1>
       </div>
       <div className="projects-container">
-        <div className="projects-format">
-          <h3>01</h3>
-          <h2>Javascript Projects</h2>
-          <p>Some basic projects using Javascript.</p>
-          <div className='projects-readmore'>
-            <p>Read More</p>
-            <img src={arrow_icon} alt="" />
+        <a href='https://github.com/anujsamdariya07/SavvyShop'>
+          <div className="projects-format">
+            <h3>01</h3>
+            <h2>SavvyShop (E-commerce Website)</h2>
+            <p>A complete full stack project with admin page</p>
+            <div className='projects-readmore'>
+              <p>Read More</p>
+              <img src={arrow_icon} alt="" />
+            </div>
           </div>
-        </div>
-        <div className="projects-format">
-          <h3>02</h3>
-          <h2>React Projects</h2>
-          <p>Some projects using React JS.</p>
-          <div className='projects-readmore'>
-            <p>Read More</p>
-            <img src={arrow_icon} alt="" />
+        </a>
+        <a href='https://github.com/anujsamdariya07/Blogging-Website'>
+          <div className="projects-format">
+            <h3>02</h3>
+            <h2>WriteWise (Blogging Website)</h2>
+            <p>A react project using Appwrite</p>
+            <div className='projects-readmore'>
+              <p>Read More</p>
+              <img src={arrow_icon} alt="" />
+            </div>
           </div>
-        </div>
-        <div className="projects-format">
-          <h3>03</h3>
-          <h2>Backend Projects</h2>
-          <p>Some projects using Node JS, Express JS, and MongoDB.</p>
-          <div className='projects-readmore'>
-            <p>Read More</p>
-            <img src={arrow_icon} alt="" />
+        </a>
+        <a href='https://github.com/anujsamdariya07/Bank-Management-System'>
+          <div className="projects-format">
+            <h3>03</h3>
+            <h2>Automated Teller Machine</h2>
+            <p>A project using JFrame library, ActionListener interface, and MySQL</p>
+            <div className='projects-readmore'>
+              <p>Read More</p>
+              <img src={arrow_icon} alt="" />
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   )
